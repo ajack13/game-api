6f71e8ba173b671f770ff075704b663b662e2a35
Ajay Srinivasan <ajay@datasisar.com>
2016-04-20 00:31:04 +0530

    ->chore : changed comment for sending mail

--- main.py
+++ main.py
@@ -13,14 +13,16 @@ from models import User,Game
 
 class SendReminderEmail(webapp2.RequestHandler):
     def get(self):
-        """Send a reminder email to each User with an email about games.
+        """Send a reminder email to User with unfinished games.
         Called every hour using a cron job"""
         app_id = app_identity.get_application_id()
         users = User.query(User.email != None)
         games = Game.query(Game.game_over == False)
         userData = {}
+        # create a dict of key:user_name value:user_email
         for user in users:
             userData[user.name] = user.email
+        # check each user for unfinished game and send a mail
         for game in games:
             if game.user_name in userData:
                 subject = 'This is a reminder!'
